641e5bc47620e93cb8c4a341b88c7f98fee93585
rocioRichi <paragit@gmail.com>
2023-03-15 13:41:57 +0100

    Delete wrong files

--- jest.confg.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['dist'],
-  resolver: 'jest-ts-webcompat-resolver',
-  collectCoverageFrom: ['src/**/*.ts'],
-  coveragePathIgnorePatterns: ['index.ts', 'app.ts', 'routers'],
-};
