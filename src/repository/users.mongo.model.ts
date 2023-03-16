a99b8f2eb97a9b4c37f08478a2e4dd2d8b1c52ca
rocioRichi <paragit@gmail.com>
2023-03-16 13:05:42 +0100

    Add model model

---
+++ src/repository/users.mongo.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+import { User } from '../entities/user.js';
+
+const userSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwd: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  telephone: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject._id;
+    delete returnedObject.password;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
